371f954e43e504016bed7ff39f4844dd8732c3f7
Dennis Zhang <81874063+ieatn@users.noreply.github.com>
2022-12-09 15:36:27 -0800

    added local storage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React, {useState} from "react"
 import "./App.css"
 import Products from "./Products"
 import Cart from "./Cart"
+import { useEffect } from "react"
+
+const cartStorage = JSON.parse(localStorage.getItem('cart'))
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(cartStorage)
   const [page, setPage] = useState('products')
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  // pass in cart array, whenever this arr changes, fire off useeffect
+  }, [cart])
 
   const getCartLength = () => {
     return cart.reduce((sum, {quantity}) => sum + quantity, 0)
@@ -14,7 +22,7 @@ function App() {
   const navigateTo = (thisPage) => {
     setPage(thisPage)
   }
-  
+
   return (
     <div className="App"> 
       {/* () => means on click, need this for buttons bc of rerender error */}
